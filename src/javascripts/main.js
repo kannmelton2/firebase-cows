f84d4cde9949a39c7fccea9007834ec6c5dd9f8e
Kayla Melton <kannmelton2@gmail.com>
2020-03-24 14:55:53 -0500

    added google login with firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,13 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+
+import auth from './components/auth/auth';
+
 import '../styles/main.scss';
 
-console.error('hello from firebase cows main js file');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  auth.loginButton();
+};
+
+init();
