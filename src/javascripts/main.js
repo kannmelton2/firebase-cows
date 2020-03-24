3118dc4223fb155a8895473aa33b14e3884ac2a0
Kayla Melton <kannmelton2@gmail.com>
2020-03-24 15:13:47 -0500

    google login showing when not authenticated, pasture showing when authenticated

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,13 @@ import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
 
 import auth from './components/auth/auth';
+import authData from './helpers/data/authData';
 
 import '../styles/main.scss';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  authData.checkLoginStatus();
   auth.loginButton();
 };
 
