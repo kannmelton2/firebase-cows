67fed3f6d74885e76f0d6f3f90ec99c04f5fda19
Kayla Melton <kannmelton2@gmail.com>
2020-03-26 14:24:55 -0500

    shows farmers on the page after login

---
+++ src/javascripts/components/farmer/farmer.js
@@ -0,0 +1,16 @@
+const farmMaker = (farmer) => {
+  let domString = '';
+  domString += '<div class="col-md-3">';
+  domString += `<div class="card" id="${farmer.id}">`;
+  domString += `<div class="card-header">${farmer.name}</div>`;
+  domString += '<div class="card-body">';
+  domString += '<h5 class="card-title"></h5>';
+  domString += `<p class="card-text">Age: ${farmer.age}</p>`;
+  domString += '</div>';
+  domString += '</div>';
+  domString += '</div>';
+
+  return domString;
+};
+
+export default { farmMaker };
--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -2,9 +2,11 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 import pasture from '../../components/pasture/pasture';
+import farmhouse from '../../components/farmhouse/farmhouse';
 
 const authDiv = $('#auth');
 const pastureDiv = $('#pasture');
+const farmhouseDiv = $('#farmhouse');
 const logoutButton = $('#navbar-logout-btn');
 
 const checkLoginStatus = () => {
@@ -13,12 +15,15 @@ const checkLoginStatus = () => {
       // person is logged in
       authDiv.addClass('hide');
       pastureDiv.removeClass('hide');
+      farmhouseDiv.removeClass('hide');
       logoutButton.removeClass('hide');
       pasture.buildCows();
+      farmhouse.buildFarmhouse();
     } else {
       // person is not logged in
       authDiv.removeClass('hide');
       pastureDiv.addClass('hide');
+      farmhouseDiv.addClass('hide');
       logoutButton.addClass('hide');
     }
   });
