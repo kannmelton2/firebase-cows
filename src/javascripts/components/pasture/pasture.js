5a9c3d33b49655bd473fc025304c36f1e149d0e9
Kayla Melton <kannmelton2@gmail.com>
2020-04-07 18:59:34 -0500

    printing new cow to page

--- src/javascripts/components/pasture/pasture.js
+++ src/javascripts/components/pasture/pasture.js
@@ -17,6 +17,27 @@ const removeCow = (e) => {
     .catch((err) => console.error('could not delete cow', err));
 };
 
+const makeACow = (e) => {
+  e.preventDefault();
+  const addNewCow = {
+    name: $('#cow-name').val(),
+    breed: $('#cow-breed').val(),
+    location: $('#cow-location').val(),
+    weight: $('#cow-weight').val() * 1,
+  };
+  console.error('newCow', addNewCow);
+  // make a new cow object
+  // save to firebase
+  cowData.addCow(addNewCow)
+    .then(() => {
+      // eslint-disable-next-line no-use-before-define
+      buildCows();
+      utils.printToDom('new-cow', '');
+    })
+    .catch((err) => console.error('add cow failed', err));
+  // reprint cows
+};
+
 const buildCows = () => {
   cowData.getCows()
     .then((cows) => {
@@ -31,6 +52,7 @@ const buildCows = () => {
       utils.printToDom('pasture', domString);
       $('body').on('click', '.delete-cow', removeCow);
       $('body').on('click', '#add-new-cow', newCow.showForm);
+      $('body').on('click', '#cow-creator', makeACow);
     })
     .catch((err) => console.error('get cows broke', err));
 };
