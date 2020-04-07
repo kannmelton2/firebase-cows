f8313db39b92815cbc3f31bc3fbda01fa358e9d1
Kayla Melton <kannmelton2@gmail.com>
2020-04-07 18:23:24 -0500

    printing new component to page

--- src/javascripts/components/pasture/pasture.js
+++ src/javascripts/components/pasture/pasture.js
@@ -1,5 +1,6 @@
 import cowData from '../../helpers/data/cowData';
 import cowComponent from '../cow/cow';
+import newCow from '../newCow/newCow';
 
 import utils from '../../helpers/utils';
 import smash from '../../helpers/data/smash';
@@ -21,6 +22,7 @@ const buildCows = () => {
     .then((cows) => {
       let domString = '';
       domString += '<h2 class="text-center">Pasture</h2>';
+      domString += '<div class="text-center"><button id="add-new-cow" class="btn btn-success m-3"><i class="fas fa-plus"></i></button></div>';
       domString += '<div class="d-flex flex-wrap">';
       cows.forEach((cow) => {
         domString += cowComponent.cowMaker(cow);
@@ -28,6 +30,7 @@ const buildCows = () => {
       domString += '</div>';
       utils.printToDom('pasture', domString);
       $('body').on('click', '.delete-cow', removeCow);
+      $('body').on('click', '#add-new-cow', newCow.showForm);
     })
     .catch((err) => console.error('get cows broke', err));
 };
